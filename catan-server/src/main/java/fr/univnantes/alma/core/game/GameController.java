d26cdf1b772fa8b8cd044a7f11cb8817af432f19
Bilal MOLLI <bilal.molli@etu.univ-nantes.fr>
2022-12-11 16:38:57 +0100

    Refactored Game to GameController

--- catan-server/src/main/java/fr/univnantes/alma/core/game/GameController.java
+++ catan-server/src/main/java/fr/univnantes/alma/core/game/GameController.java
@@ -1,12 +1,10 @@
 package fr.univnantes.alma.core.game;
 
 import fr.univnantes.alma.core.commands.Command;
-import fr.univnantes.alma.core.controler.ServerGameController;
+import fr.univnantes.alma.core.gamemager.ServerGameController;
 import fr.univnantes.alma.core.game.entity.Player;
 
-import java.util.List;
-
-public interface Game{
+public interface GameController {
     Player joinPlayer(Command command);
     boolean isFull();
 
