d539c41a5cfdbadb1b65b3105fb21b972989ee80
Rod4401 <m.meunier.rodrigue@gmail.com>
2022-12-11 19:50:36 +0100

    Update Map.java

--- catan-server/src/main/java/fr/univnantes/alma/core/game/map/Map.java
+++ catan-server/src/main/java/fr/univnantes/alma/core/game/map/Map.java
@@ -4,13 +4,35 @@ import fr.univnantes.alma.core.game.map.coordinates.Coordinates;
 import fr.univnantes.alma.core.game.map.tile.Tile;
 
 public interface Map {
+	/**
+	 * Generation of tiles
+	 */
 	public void generateTiles();
 
+	/**
+	 * Generation of vertices and edges
+	 */
 	public void generateEdgesVertices();
 
+	/**
+	 * Harbor generation
+	 */
 	public void placeHarbors();
 
+	/**
+	 * Getter of an Tile
+	 * 
+	 * @exception IllegalArgumentException Throw if the coordinates are not correct
+	 * @param coords The coordinates
+	 * @return The tile
+	 */
 	public Tile getTile(Coordinates coords);
 
+	/**
+	 * Test if coords are correct
+	 * 
+	 * @param coords The coords
+	 * @return true or false
+	 */
 	public boolean isValidCoordinates(Coordinates coords);
 }
