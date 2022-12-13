052be5e1b17874eb14bc179dc092a120a27a8747
Bilal Molli <bilal.molli@etu-univnantes.com>
2022-12-13 16:49:41 +0100

    handle polymorphism in jackson

--- catan-server/src/main/java/fr/univnantes/alma/server/commandsimpl/JacksonCommandWithContext.java
+++ catan-server/src/main/java/fr/univnantes/alma/server/commandsimpl/JacksonCommandWithContext.java
@@ -1,11 +1,14 @@
 package fr.univnantes.alma.server.commandsimpl;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import fr.univnantes.alma.core.commands.Command;
 import fr.univnantes.alma.core.commands.CommandWithContext;
 import fr.univnantes.alma.core.game.entity.Player;
 
 import java.util.Set;
 
-public abstract class JacksonCommandWithContext extends CommandWithContext implements JacksonSerialisableCommand {
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.WRAPPER_OBJECT, property = "command")
+public abstract class JacksonCommandWithContext extends CommandWithContext implements Command {
     protected JacksonCommandWithContext(Set<Player> recipient) {
         super(recipient);
     }
--- catan-server/src/main/java/fr/univnantes/alma/server/commandsimpl/JacksonSerialisableCommand.java
+++
@@ -1,6 +0,0 @@
-package fr.univnantes.alma.server.commandsimpl;
-
-import fr.univnantes.alma.core.commands.Command;
-
-
-public interface JacksonSerialisableCommand extends Command {}
