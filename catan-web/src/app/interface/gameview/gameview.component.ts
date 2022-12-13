937d98fbb5934ecd66ffbe98bc288a41f8eec977
theray1 <theray1@outlook.fr>
2022-12-13 12:10:09 +0100

    minor addition

--- catan-web/src/app/interface/gameview/gameview.component.ts
+++ catan-web/src/app/interface/gameview/gameview.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Tile } from 'src/app/tile';
 
 @Component({
   selector: 'app-gameview',
   templateUrl: './gameview.component.html',
   styleUrls: ['./gameview.component.css']
 })
-export class GameviewComponent {
+export class GameviewComponent implements OnInit {
+
+  constructor(){}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  board: Tile[] = [];
+
+  
 
 }
